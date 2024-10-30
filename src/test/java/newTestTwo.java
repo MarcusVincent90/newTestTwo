ebb20457e5b503e07e0b43a750492e66512ef122
marcus.arvidsson <marcus.v.arvidsson@gmail.com>
2024-10-30 17:33:09 +0100

    Test

---
+++ src/test/java/newTestTwo.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class newTestTwo {
+
+    @Test
+    public void newTestTwo(){
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void newTestTwoTwo(){
+        Assert.assertTrue(false);
+    }
+}
